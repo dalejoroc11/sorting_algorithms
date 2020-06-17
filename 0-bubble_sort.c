24a47fc131a07a8c86d54235320128c0b08dc4a4
dalejoroc11 <diegorojas279@hotmail.com>
2020-06-17 02:40:52 +0000

    fix tasks

---
+++ 0-bubble_sort.c
@@ -0,0 +1,25 @@
+#include "sort.h"
+/**
+ * bubble_sort - sorts array in ascending order
+ * @array : array whit pointer
+ * @size : size int
+ */
+void bubble_sort(int *array, size_t size)
+{
+  int temp = 0;
+  size_t i, j;
+
+  for (i = 0; i < size; i++)
+    {
+      for (j = 0; j < size - 1 - i; j++)
+	{
+	  if (array[j] > array[j + 1])
+	    {
+	      temp = array[j + 1];
+	      array[j + 1] = array[j];
+	      array[j] = temp;
+	      print_array(array, size);
+	    }
+	}
+    }
+}
