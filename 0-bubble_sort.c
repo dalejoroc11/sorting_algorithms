91ef1ec485adceb214ad8b2bb082f3b36a26b7b5
dalejoroc11 <diegorojas279@hotmail.com>
2020-06-17 23:10:38 +0000

    fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -3,11 +3,14 @@
  * bubble_sort - sorts array in ascending order
  * @array : array whit pointer
  * @size : size int
+ * Return: no
  */
 void bubble_sort(int *array, size_t size)
 {
 int temp = 0;
 size_t i, j;
+if (array == NULL || size == 0)
+return;
 for (i = 0; i < size; i++)
 {
 for (j = 0; j < size - 1 - i; j++)
